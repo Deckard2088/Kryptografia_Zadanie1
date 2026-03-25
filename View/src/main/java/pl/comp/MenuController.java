82555074da9698be2f1650bd6ad519ffdcdbc7ca
Dawid <dawid.wachecki@onet.pl>
2026-03-25 12:37:11 +0100

    Lekka zmiana w View + dodane id w fxml

---
+++ View/src/main/java/pl/comp/MenuController.java
@@ -0,0 +1,12 @@
+package pl.comp;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class MenuController extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
