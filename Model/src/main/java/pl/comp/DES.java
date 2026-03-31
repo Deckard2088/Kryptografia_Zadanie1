7ef3c484bf54d81566c414566318e5e84be2dc55
Dawid <dawid.wachecki@onet.pl>
2026-03-31 14:26:48 +0200

    feature: nowa funkcja z permutacją bitów

--- Model/src/main/java/pl/comp/DES.java
+++ Model/src/main/java/pl/comp/DES.java
@@ -3,16 +3,14 @@ package pl.comp;
 import java.nio.charset.StandardCharsets;
 
 public class DES {
+    //możnaby nazwy zmienić bo nwm czy później z czymś kolidować nie będą
     private static final byte[] schemaForPC1 = {
             57, 49, 41, 33, 25, 17, 9,
             1 ,58, 50, 42, 34, 26, 18,
             10, 2, 59, 51, 43, 35, 27,
-            19, 11, 3, 60, 52, 44, 36
-    };
-
-    private static final byte[] schemaForPC2 = {
+            19, 11, 3, 60, 52, 44, 36,//tymczasowe rozszerzenie bo cos sprawdzałem
             63, 55, 47, 39, 31, 23, 15,
-             7, 62, 54, 46, 38, 30, 22,
+            7, 62, 54, 46, 38, 30, 22,
             14, 6, 61, 53, 45, 37, 29,
             21, 13, 5, 28, 20, 12, 4
     };
@@ -32,6 +30,22 @@ public class DES {
                 finalKey[i] = 0;
             }
         }
-        return finalKey;
+        byte[] finalfinal = new byte[8];
+        finalfinal = bitPermutation(finalKey, schemaForPC1);
+        return finalfinal;
+    }
+
+    public byte[] bitPermutation(byte[] byteTable, byte[] positions){
+        byte[] permutatedBytetable = new byte[7];
+        for (int i = 0; i < positions.length; i++){
+            //pobieramy z którego bajtu chcemy wyciągnąc bit (np. dla 57. bitu to 6 bajt - przez to że ignorujemy bity parzystości)
+            byte byteNumber = (byte) ((positions[i] - 1) / 8);
+            //pobieramy który bit chcemy wyjąć w ramach tego bajtu (np. dla 57. bitu to 1 bit 7 bajtu)
+            byte bitNumber = (byte) (7 - ((positions[i] - 1) % 8));
+            //wartość bitu (0 lub 1)
+            byte valueOfBit = (byte) ((byteTable[byteNumber] >> bitNumber) & 1);
+            permutatedBytetable[i/8] = (byte) (permutatedBytetable[i/8] | (valueOfBit << bitNumber));
+        }
+        return permutatedBytetable;
     }
 }
